4a5c9669ee994b5250debc9a571c8269241294ce
Marek Wolski <marek.wolski@gmail.com>
2024-04-06 12:24:50 +0100

    Some spike refactoring plus an output report file
    Some refactoring of the current spike work.
    Added the creation of a links report file to be able to see the current output and results more clearly. Links report file added to .gitignore since this doesn't need to be included in the repo.

--- spikes.py
+++ spikes.py
@@ -38,16 +38,19 @@ def search_text_file(filepath, pattern):
             if found:
                 for inst in found:
                     matches.append(inst)
-    return matches
+    return matches # a List of Tuples containing any markdown links to image attachments
 
 def find_matches(filelist, pattern):
+    matches = []
     for file in filelist:
-        matches = search_text_file(file, pattern)
-        if matches:
-            print(f"\nFound {len(matches)} matches in {file}:")
-            print(matches)
-        else:
-            print("No matches found for the pattern:", pattern)
+        match = search_text_file(file, pattern)
+        if match:
+            #print(f"\nFound {len(match)} matches in {file}:")
+            #print(match)
+            matches.append(match)
+        #else:
+            #print("No matches found for the pattern:", pattern)
+    return matches
 
 
 if __name__ == "__main__":
@@ -57,11 +60,21 @@ if __name__ == "__main__":
     file_type = "*.md"  # Example: Find all MD files
     pattern = r"\[\[(.*?)(\.jpg|\.jpeg|\.png)(.*?)\]\]"
 
+# Seek through the Vault, finding all the note files
+# and look inside each one for links to image attachments
 
     found_files = find_files(root_dir, file_type)
     if found_files:
 #        print("Found files:")
-         find_matches(found_files, pattern)
+         found_matches = find_matches(found_files, pattern)
+         # will be a list of lists, with each element containing one or more tuples containing the matched file info 
+         if found_matches:
+            with open(f'links_report.txt', 'w') as linksfile:
+                for m in found_matches:
+                    for inst in m:
+                        linksfile.write(str(inst))
+                        linksfile.write('\n\n')
+                        #print(inst)
     else:
         print("No files of type", file_type, "found in", root_dir)
 
